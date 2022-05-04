cede75a2f347bfb6f01e9c490c0d34d4f44b5f3c
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2022-05-04 18:40:43 +0200

    extract frames done

--- extract.ts
+++ extract.ts
@@ -7,29 +7,38 @@ import ffmpeg from 'ffmpeg';
 // captures frames from a video, name them and copy them to specified path
 const extractFramesFromVideo = (
   videoPath: string,
+  videoName: string,
   imagesPath: string,
   frameRate: number
-) => {
-  var process = new ffmpeg(videoPath);
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const process = new ffmpeg(videoPath + videoName);
 
-  process.then(
-    function (video) {
-      // Callback mode
-      video.fnExtractFrameToJPG(
-        `${imagesPath}/`,
-        {
-          frame_rate: frameRate,
-          file_name: videoPath + '_%t_%s',
-        },
-        function (error, files) {
-          if (!error) console.log('Frames: ' + files);
-        }
-      );
-    },
-    function (err) {
-      console.log('Error: ' + err);
-    }
-  );
+    process.then(
+      function (video) {
+        // Callback mode
+        video.fnExtractFrameToJPG(
+          `${imagesPath}/`,
+          {
+            frame_rate: frameRate,
+            file_name: videoName.split('.mov')[0] + '_%t_%s',
+          },
+          function (error, files) {
+            if (!error) {
+              resolve('ok');
+            } else {
+              console.log(error);
+              reject('Error during extract');
+            }
+          }
+        );
+      },
+      function (err) {
+        console.log('Error: ' + err);
+        reject('Error when processing video');
+      }
+    );
+  });
 };
 
 export default extractFramesFromVideo;
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import extract from './extract.js';
 import glob from 'glob';
+const fs = require('fs');
 
 try {
   // gets files from video repertory
@@ -10,11 +11,18 @@ try {
       .map((file) => {
         // process file
         extract(
-          `${process.env.VIDEO_PATH}/${file}`,
+          `${process.env.VIDEO_PATH}`,
+          `${file.split('/')[file.split('/').length - 1]}`,
           `${process.env.IMAGES_PATH}/`,
           1
-        );
-        // rename file to mark it processed
+        )
+          .then((result: string) => {
+            // rename file to mark it processed
+            fs.rename(file, `${file.split('.mov')[0]}_processed.mov`, () => {
+              console.log(`${file} processed`);
+            });
+          })
+          .catch((error: string) => console.log(error));
       });
   });
 } catch (e) {
